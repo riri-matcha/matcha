5eef6b761676ddfd05524bf787d22cf0e4222565
Raihanna Rahmati <raihannarahmati94@gmail.com>
2025-07-31 18:04:58 +0200

    Add vercel.json and simplify homepage for testing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ec7dbb89be24812"; }
+/******/ 		__webpack_require__.h = function() { return "36a1c2dcae60eeb2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
